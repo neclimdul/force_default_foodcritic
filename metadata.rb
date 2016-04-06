1a840a6a2e95dcfde029ee8d508d4ded86341afc
James Gilliland <jgilliland@apqc.org>
2016-04-05 22:51:52 -0500

    Clean up unrelated foodcritic messages

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,5 @@ license 'All rights reserved'
 description 'Installs/Configures apqc_drupal'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
-
+source_url 'https://github.com/neclimdul/force_default_foodcritic'
+issues_url 'https://github.com/neclimdul/force_default_foodcritic/issues'
